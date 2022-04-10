e5d119ae51e3a55ed4e681bf339d12e5fdc45663
ChuSeungDeok <cntmdejrsla@naver.com>
2022-04-10 17:24:00 +0900

    검색
    리스트 아이템 구현,
    검색 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 
-import './App.css';
-import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import React, { useEffect, useState } from 'react';
 import VideoList from './component/video_list/video_list';
+import SearchHeader from './component/search_header/search_header';
 
-function App() {
+
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-
-  useEffect(()=>{},[]);
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+  const search = query => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   };
+    
+  useEffect(()=>{
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
+  },[]);
   
-  fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDDA8k-AjOb6kTaagNTUPJGC1QxSQrrxhc", requestOptions)
-    .then(response => response.json())
-    .then(result => setVideos(result.items))
-    .catch(error => console.log('error', error));
-  
-  
-    return <VideoList videos={videos} />;
-  
+
+  return (
+  <div className={styles.app}>
+    <SearchHeader onSearch={search}/>
+    <VideoList videos={videos}/>
+  </div>
+  );
   
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Youtube from './service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
